7b08221133149d9a07fa1d1eb4109386d16f8cb0
ainur1313 <ainur_bel@mail.ru>
2026-07-15 10:27:37 +0300

    task10

---
+++ Plugins/DependentPlugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CommandLib;
+
+namespace TestPlugin2
+{
+    [PluginLoad("SimplePlugin")]
+    public class DependentPlugin : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("DependentPlugin");
+            Console.WriteLine("Зависит от SimplePlugin");
+
+        }
+    }
+}
---
+++ Plugins/SimplePlugin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CommandLib;
+
+namespace TestPlugin1
+{
+    [PluginLoad("")]
+    public class SimplePlugin : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("SimplePlugin");
+        }
+    }
+}
