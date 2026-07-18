060824eee47cdf7601ecd56be99106c46497ddf9
ainur1313 <ainur_bel@mail.ru>
2026-07-18 16:11:24 +0300

    task17

---
+++ task17/ServerThread.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using task17;
+
+namespace task17
+{
+    public class ServerThread
+    {
+        private readonly Thread _thread;
+        private readonly BlockingCollection<ICommand> _queue = new BlockingCollection<ICommand>();
+        private volatile bool _hardStopRequested;
+
+        public ServerThread()
+        {
+            _thread = new Thread(Run)
+            {
+                IsBackground = true
+            };
+        }
+
+        public void Start()
+        {
+            _thread.Start();
+        }
+
+        public void AddCommand(ICommand command)
+        {
+            _queue.Add(command);
+        }
+
+        private void Run()
+        {
+            foreach (var command in _queue.GetConsumingEnumerable())
+            {
+                try
+                {
+                    command.Execute();
+                }
+                catch (ThreadInterruptedException)
+                {
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    ExceptionHandler.Handle(command, ex);
+                }
+
+                if (_hardStopRequested)
+                    break;
+            }
+        }
+
+        public void HardStop()
+        {
+            if (Thread.CurrentThread != _thread)
+                throw new InvalidOperationException(
+                    "HardStop must be called from the server thread");
+
+            _hardStopRequested = true;
+            _thread.Interrupt();
+        }
+
+        public void SoftStop()
+        {
+            if (Thread.CurrentThread != _thread)
+                throw new InvalidOperationException(
+                    "SoftStop must be called from the server thread");
+
+            _queue.CompleteAdding();
+        }
+    }
+}
---
+++ task17tests/ServerThreadTests.cs
@@ -0,0 +1,74 @@
+﻿using System.Threading;
+using task17;
+using Xunit;
+
+namespace task17tests
+{
+    public class ServerThreadTests
+    {
+        [Fact]
+        public void SoftStop_Should_ProcessAllCommands_BeforeStopping()
+        {
+            var server = new ServerThread();
+            int counter = 0;
+
+            server.AddCommand(new TestCommand(() => Interlocked.Increment(ref counter)));
+            server.AddCommand(new TestCommand(() => Interlocked.Increment(ref counter)));
+            server.AddCommand(new SoftStopCommand(server));
+
+            server.Start();
+            Thread.Sleep(2000);
+
+            Assert.Equal(2, counter);
+        }
+
+        [Fact]
+        public void HardStop_Should_InterruptProcessing_Immediately()
+        {
+            var server = new ServerThread();
+            int counter = 0;
+
+            server.AddCommand(new TestCommand(() => Interlocked.Increment(ref counter)));
+            server.AddCommand(new HardStopCommand(server));
+            server.AddCommand(new TestCommand(() => Interlocked.Increment(ref counter)));
+
+            server.Start();
+            Thread.Sleep(2000);
+
+            Assert.Equal(1, counter);
+        }
+
+        [Fact]
+        public void HardStop_Should_Throw_WhenCalledFromWrongThread()
+        {
+            var server = new ServerThread();
+            server.Start();
+
+            Assert.Throws<InvalidOperationException>(() => server.HardStop());
+        }
+
+        [Fact]
+        public void SoftStop_Should_Throw_WhenCalledFromWrongThread()
+        {
+            var server = new ServerThread();
+            server.Start();
+
+            Assert.Throws<InvalidOperationException>(() => server.SoftStop());
+        }
+    }
+
+    internal class TestCommand : ICommand
+    {
+        private readonly System.Action _action;
+
+        public TestCommand(System.Action action)
+        {
+            _action = action;
+        }
+
+        public void Execute()
+        {
+            _action();
+        }
+    }
+}
